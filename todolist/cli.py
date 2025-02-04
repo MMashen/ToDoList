7eb43a288f708189ea8be114d397f94ef75da8a6
MMashencheng <mm.cheng@swag.live>
2025-02-04 15:32:21 +0800

    ex1

--- todolist/cli.py
+++ todolist/cli.py
@@ -6,7 +6,7 @@ from todolist import ERRORS,__app_name__,__version__,config,database
 
 import typer
 
-from todolist import (
+from todolist impor1t (
     __app_name__ ,
     __version__,
     ERRORS,
